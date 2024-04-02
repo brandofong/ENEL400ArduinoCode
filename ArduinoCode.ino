1905f31fc9a1e4be9200fd3fe4f3d498638438cc
brandofong <108887808+brandofong@users.noreply.github.com>
2024-04-02 17:14:10 -0600

    Update ArduinoCode.ino
    Fixed bugs and added watchdog timer so it can reset itself.

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -1,6 +1,7 @@
   #include <SPI.h>
   #include <SD.h>
   #include <AudioZero.h>
+  #include <Adafruit_SleepyDog.h>
 
   volatile bool interruptOccurred = false; // Shared variable between ISR and loop
   bool audioInitialized = false; //Sets the flag if AudioZero has been intialized already so it doesn't reinitialize it
@@ -15,10 +16,9 @@
   const int echoPin = 10;
 
 
-
   void setup() {
     // Open serial communications and wait for port to open:
-    Serial.begin(9600);
+    Serial.begin(115200);
 
   pinMode(inputPinSide, INPUT); // Set the input pin 6
   pinMode(inputPinDist, INPUT); // Set the input pin 7
@@ -28,6 +28,7 @@
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  
 
     // see if the card is present and can be initialized:
     if (!SD.begin(chipSelect)) {
@@ -36,10 +37,8 @@
       while (1);
     }
 
-  // 44100kHz stereo => 88200 sample rate
-
-    AudioZero.begin(2*44100); // start sampling
-    
+    AudioZero.begin(44100); // start sampling
+    Watchdog.enable(2000); // Set the timer to be 2 seconds incase no reset then reset arduino
 
   }
 
@@ -69,93 +68,72 @@
   //  Start speaker code
 
   if (digitalRead(inputPinPeople) == HIGH || ( cm > 0 && cm < 200)) { // (1) If pin 8 is high, there is nobody in the frame and audio playback will not play (100)
-
-      AudioZero.end(); // "Stops" the DAC0 pin from outputting noise
-      audioInitialized = false; // Reset the flag
-  
-  if (!audioInitialized){
-// Do nothing in this if the audio is already unitialized 
-  }
-else {
-        Serial.print(cm);
-        Serial.print("cm");
-        Serial.println();
-    }
+    AudioZero.end(); // "Stops" the DAC0 pin from outputting noise
+    audioInitialized = false; // Reset the flag
+    Watchdog.reset(); //reset the watchdog timer
+ 
 }
 
     
 
   else if (digitalRead(inputPinSide) == HIGH && digitalRead(inputPinDist) == HIGH) { // (2) If pin on 6 and 7 are high, they are RIGHT side and CLOSE (3m -7m) (011)
 
-    Serial.println("Pin 6 and 7 are high, they are RIGHT side and CLOSE (3m -7m)");
-        if (!audioInitialized ) { // If audio hasn't be initialized yet, then do it (To prevent double initializing)
-
-      AudioZero.begin(2* 44100); // Corresponds to the 44.1kHz output of audio sampling at 88.2kHz
-
+    if (!audioInitialized ) { // If audio hasn't be initialized yet, then do it (To prevent double initializing)  
+      AudioZero.begin(44100); // Corresponds to the 44.1kHz output of audio sampling at 88.2kHz
       audioInitialized = true; // Set flag to indicate audio initialization
 
     }
-    
     File myFile = SD.open("fast1.wav");     // open wave file from sdcard
     AudioZero.play(myFile);
     myFile.close(); // Close file to prevent leaks
+    Watchdog.reset();
 
     }
 
     else if (digitalRead(inputPinSide) == HIGH && digitalRead(inputPinDist) == LOW) { // (3) If pin on 6 is high and 7 is low, they are RIGHT side and FAR 7m< (001)
 
-    Serial.println("If pin on 6 is high and 7 is low, they are RIGHT side and FAR 7m<");
-        if (!audioInitialized) { // If audio hasn't be initialized yet, then do it (To prevent double initializing)
-
-      AudioZero.begin(2* 44100); // Corresponds to the 44.1kHz output of audio
-     
-      audioInitialized = true; // Set flag to indicate audio initialization
+    if (!audioInitialized) { // If audio hasn't be initialized yet, then do it (To prevent double initializing)
+    AudioZero.begin(44100); // start sampling
+    audioInitialized = true; // Set flag to indicate audio initialization
 
     }
-
     File myFileTwo = SD.open("slow1.wav");     // open wave file from sdcard
     AudioZero.play(myFileTwo);
     myFileTwo.close();
+    Watchdog.reset();
 
     }
 
 
   else if (digitalRead(inputPinSide) == LOW && digitalRead(inputPinDist) == HIGH) { //(4) If pin on 6 is LOW and 7 is HIGH, they are LEFT side and FAR (7m<) 
 
-    Serial.println("If pin on 6 is LOW and 7 is HIGH, they are LEFT side and CLOSE (3m -7m)");
       if (!audioInitialized) { // If audio hasn't be initialized yet, then do it (To prevent double initializing)
-
-      AudioZero.begin(2* 44100); // Corresponds to the 44.1kHz output of audio sampling at 88.2kHz
-     
+      AudioZero.begin(44100);
       audioInitialized = true; // Set flag to indicate audio initialization
 
     }
     
     File myFile = SD.open("faster.wav");     // open wave file from sdcard
-      // File myFile = SD.open("fastt.wav");     // open wave file from sdcard
-
     AudioZero.play(myFile);
     myFile.close();
-    }
-
-  else { // 
-
-    Serial.println("Pin 6 and 7 are both LOW. (Left Side and FAR)");
-        if (!audioInitialized) { // If audio hasn't be initialized yet, then do it (To prevent double initializing)
-
-      AudioZero.begin(2* 44100); // Corresponds to the 44.1kHz output of audio sampling at 88.2kHz
- 
-      audioInitialized = true; // Set flag to indicate audio initialization
+    Watchdog.reset();
 
     }
-    
-    // File myFile = SD.open("faster.wav");     // open wave file from sdcard
-      File myFile = SD.open("fastt.wav");     // open wave file from sdcard
 
+  else {
+
+    if (!audioInitialized) { // If audio hasn't be initialized yet, then do it (To prevent double initializing)
+      
+    AudioZero.begin(44100); // Corresponds to the 44.1kHz output of audio sampling at 88.2kHz
+    audioInitialized = true; // Set flag to indicate audio initialization
+
+    }
+    File myFile = SD.open("fastt.wav");     // open wave file from sdcard
     AudioZero.play(myFile);
     myFile.close();
-
+    Watchdog.reset();
     }
+
   }
 
   long microsecondsToCentimeters(long microseconds) {
